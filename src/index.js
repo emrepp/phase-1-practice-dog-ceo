17346080bf4d7a90bbd900f0886854a47469a043
Emma Repp <emmarepp.draws@gmail.com>
2022-07-20 22:14:15 -1000

    commit tree b1f31d18d6375d16873e273e4118d7f836494b09

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 console.log('%c HI', 'color: firebrick')
+const container = document.querySelector('#dog-image-container')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function getImages(){
+fetch(imgUrl)
+.then(resp => resp.json())
+.then(images => {
+    const imgs = images.message
+    let imgsArray = createImgElement(imgs)
+    renderImgs(imgsArray)
+})}
+ 
+        function createImgElement(imgs){
+        return imgs.map((img) => {
+            let i = `<img src=${img}>`
+            return i
+        })
+       
+    }
+    function renderImgs(imgsArray){
+        imgsArray.forEach(element =>{
+            renderImgs(element)
+        })}
+
+    
+    function renderImg(element){
+        container.innerHTML += element
+    }
+
+    getImages()
+
+
